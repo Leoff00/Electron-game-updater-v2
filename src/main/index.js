7ce366d34becc4312f11c1fa9e51f713120b056f
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2022-10-24 22:22:03 -0300

    feat: bumping deps + refactoring code to work with new deps

--- src/main/index.js
+++ src/main/index.js
@@ -1,54 +1,65 @@
 "use strict";
+
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import { format as formatUrl } from "url";
 
-// eslint-disable-next-line no-unused-vars
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+// global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow;
 
 function createMainWindow() {
   const window = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    width: 800,
-    height: 600,
-    transparent: true,
-    frame: true,
-    fullscreenable: false,
-    maximizable: false,
-    resizable: false,
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
 
-  let url = formatUrl({
-    pathname: path.join(__dirname, "index.html"),
-    protocol: "file",
-    slashes: true,
-  });
+  if (isDevelopment) {
+    window.webContents.openDevTools();
+  }
 
-  window.on("error", (error) => {
-    console.error({
-      error,
-    });
-  });
+  if (isDevelopment) {
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+  } else {
+    window.loadURL(
+      formatUrl({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
+        slashes: true,
+      })
+    );
+  }
 
   window.on("closed", () => {
     mainWindow = null;
   });
 
-  window.loadURL(url);
+  window.webContents.on("devtools-opened", () => {
+    window.focus();
+    setImmediate(() => {
+      window.focus();
+    });
+  });
 
   return window;
 }
 
-function runApp() {
-  mainWindow = createMainWindow();
-}
-
-app.on("ready", () => {
-  runApp();
+// quit application when all windows are closed
+app.on("window-all-closed", () => {
+  // on macOS it is common for applications to stay open until the user explicitly quits
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-if (module.hot) {
-  module.hot.accept();
-}
+app.on("activate", () => {
+  // on macOS it is common to re-create a window even after all windows have been closed
+  if (mainWindow === null) {
+    mainWindow = createMainWindow();
+  }
+});
+
+// create main BrowserWindow when electron is ready
+app.on("ready", () => {
+  mainWindow = createMainWindow();
+});
--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,19 +1,3 @@
-const merge = require("webpack-merge");
-
-module.exports = function (config) {
-  config = merge.smart(config, {
-    output: {
-      publicPath: "",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: /node_modules/,
-        },
-      ],
-    },
-  });
-
-  return config;
+module.exports = {
+  devtool: "nosources-source-map",
 };
